dbbd3fa80da7140912651d8610d78e11f3b60187
Pankorn Chantarod <pankorn.ch@gmail.com>
2021-10-10 02:51:24 +0700

    :lipstick: imagePicker radioGroup

---
+++ lib/shared/DatePicker.dart
@@ -0,0 +1,72 @@
+import 'package:flirtii/configs/constants.dart';
+import 'package:flirtii/services/dateFormat.dart';
+import 'package:flutter/material.dart';
+
+class DatePicker extends StatefulWidget {
+  final String? title;
+  final String? value;
+  final Function? onChange;
+
+  const DatePicker({
+    Key? key,
+    this.title,
+    this.value,
+    this.onChange,
+  }) : super(key: key);
+
+  @override
+  _DatePickerState createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () async {
+        final selected = await showDatePicker(
+          context: context,
+          initialDate: selectedDate,
+          firstDate: DateTime(2010),
+          lastDate: DateTime(2025),
+        );
+
+        if (selected != null && selected != selectedDate)
+          setState(() {
+            selectedDate = selected;
+          });
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          widget.title != null
+              ? Column(
+                  children: [
+                    Text("Date of Birth",
+                        style: TextStyle(
+                          fontSize: 16,
+                        )),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                )
+              : SizedBox(),
+          Row(
+            children: [
+              Icon(
+                Icons.calendar_today,
+                color: kMainPinkColor,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(DateFormat(selectedDate).dateToString()),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
